6b9225004052c3db141f519b5fad30eacf543d7e
An B. Luu <davidpro1080@gmail.com>
2024-11-10 16:09:18 -0800

    finished UVR

--- src/fastAPI/uvr.py
+++ src/fastAPI/uvr.py
@@ -1,11 +1,15 @@
 from audio_separator.separator import Separator
 
 DOWNLOADS_FOLDER = "./downloads"
+OUTPUT_FOLDER = "./downloads/instrumental"
+
+import os
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 # Initialize the Separator class (with optional configuration properties, below)
 separator = Separator(
     model_file_dir="/tmp/audio-separator-models/",
-    output_dir=DOWNLOADS_FOLDER,
+    output_dir=OUTPUT_FOLDER,
     output_format="mp3",
     normalization_threshold=0.9,
     output_single_stem="Instrumental",
@@ -22,7 +26,16 @@ separator = Separator(
 # Load Model
 separator.load_model(model_filename="UVR-MDX-NET-Inst_HQ_3.onnx")
 
-# Perform the separation on specific audio files without reloading the model
-output_files = separator.separate('downloads/The Real Slim Shady.mp3')
+# Takes in file path, generate an instrumental file and return its file_path
+def separate_audio(file_path):
+    output_file = separator.separate(file_path)
+    # print(f"Separation complete! Output file(s): {' '.join(output_files)}")
+    print("separation complete")
+    print(output_file)
+    return output_file
 
-print(f"Separation complete! Output file(s): {' '.join(output_files)}")
+
+
+
+# # Perform the separation on specific audio files without reloading the model
+# output_files = separator.separate('downloads/The Real Slim Shady.mp3')
