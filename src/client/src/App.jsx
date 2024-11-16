1abfe917003dd7cfbc01295b14b6666774b27f90
An B. Luu <davidpro1080@gmail.com>
2024-11-15 16:51:35 -0800

    Created Karaoke page and Added routes and links to Karaoke page

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,13 +1,21 @@
+import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Karaoke from "./KaraokePage.jsx";
 
 const App = () => {
   return (
-    <div className="w-full p-6">
-      <Navbar />
-      <Home />
-      
-    </div>
+    <Router>
+      <div className="w-full p-6">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/karaoke" element={<Karaoke />} />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 };
 export default App
---
+++ src/client/src/KaraokePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Karaoke = () => {
+  return (
+    <div>
+      <h1>Karaoke Page</h1>
+    </div>
+  );
+};
+
+export default Karaoke;
