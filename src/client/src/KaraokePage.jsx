82763356004c9875072b6fbb38863b75d7632af9
Parsa Ghaderi <parsaghaderi1381@gmail.com>
2024-11-16 12:18:33 -0800

    made Lyricsplayer and videooverlay compponts

--- src/client/src/KaraokePage.jsx
+++ src/client/src/KaraokePage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import SideNavbar from './components/SideNavbar';
-
+import LyricsDisplay from './components/LyricsDisplay.jsx';
 const Karaoke = () => {
   return (
-      <div className="flex h-screen">
-          {/** Lyrics and music player area */}
-          <div className="flex-1 bg-gray-100 p-8">
-              <h1 className="text-2xl font-bold">Lyrics Display</h1>
-            
-        </div>
-          
-          {/** Navigation bar */}
-          <SideNavbar/>
+    <div className="flex h-screen">
+      {/** Lyrics and music player area */}
+      <div className="flex-1 bg-gray-100 p-8">
+        <h1 className="text-2xl font-bold">Lyrics Display</h1>
+
       </div>
+
+      {/** Navigation bar */}
+      <SideNavbar />
+    </div>
   );
 };
 
