34e589a5c78b28e77f73764d3646315efeac2072
An B. Luu <davidpro1080@gmail.com>
2024-11-21 15:29:06 -0800

    added function to get audio

--- src/fastAPI/main.py
+++ src/fastAPI/main.py
@@ -61,10 +61,9 @@ spotify = SpotifyDIY(env_file="python.env")
 async def search_songs(search_query: SearchQuery):
     try:
         track_list = spotify.get_tracks(search_query.query)
-        
+        song_obj_list = []
         if not track_list:
             raise HTTPException(status_code=404, detail="No tracks found for the given query.")
-        song_obj_list = []
         for track in track_list:
             try:
                 # Create a Song object
@@ -72,16 +71,17 @@ async def search_songs(search_query: SearchQuery):
 
                 # Append the song object to the list
                 song_obj_list.append(song)
-
+                
             except Exception as e:
                 logger.error(f"Error processing track {track.get('name', 'Unknown Track')}: {e}")
             
-        return {"status": "success", "songs": song_obj_list}
+        return song_obj_list
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
         logger.error(f"Unexpected error: {e}")
         raise HTTPException(status_code=500, detail="An unexpected error occurred.")
+
 
 @app.post("/fetch-song")
 async def fetch_song(song: Song):
@@ -103,14 +103,26 @@ async def fetch_song(song: Song):
         raise HTTPException(status_code=500, detail=f"Error processing the song: {str(e)}")
 
 @app.post("/get-audio")
+async def get_audio(song: Song):
+    """
+    Receives a song object with an empty instrumental_url.
+    Generates the instrumental file if it doesn't exist, and returns the URL.
+    """
+    try:
+        # Check if the song already has an instrumental file
+        if song.instrumental_URL:
+            return song.instrumental_URL
+        song.get_audio()
+        return song.instrumental_URL
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing audio: {str(e)}")
 
-@app.post("/check-lyrics-available")
 
-@app.post("/get-audio")
 
-@app.get("/delete-song")
-    #takes a query
-    # Deletes files associated with that song
+# @app.get("/delete-song")
+#     #takes a query
+#     # Deletes files associated with that song
 
 
 
