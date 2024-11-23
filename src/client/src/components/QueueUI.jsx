2b4389f2cd1dee7ec3d1cc6a1df73deeddeb92db
Caiden <caiden.merklin@gmail.com>
2024-11-23 15:25:40 -0800

    Updated QueueUI

--- src/client/src/components/QueueUI.jsx
+++ src/client/src/components/QueueUI.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
 const QueueUI = () => {
   return (
-    <div>QueueUI</div>
-  )
-}
+    <div>
+      <h2 className="text-lg font-bold">Queue</h2>
+      <ul>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+        {/* Add more items as needed */}
+      </ul>
+    </div>
+  );
+};
 
-export default QueueUI
+export default QueueUI;
