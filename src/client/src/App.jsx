c1010bbb9cfd286788b00b08f6e095bc9074e6e0
An B. Luu <davidpro1080@gmail.com>
2024-11-23 15:37:31 -0800

    added QueueContext and integrated that into App.jsx

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -5,19 +5,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Karaoke from "./KaraokePage.jsx";
 import ProgressBar from "./components/ProgressBar.jsx";
 import LyricsDisplay from "./components/LyricsDisplay.jsx";
+import { QueueProvider } from "./context/QueueContext"; // Import QueueProvider
 
 const App = () => {
   return (
-    <Router>
-      <div className="w-full p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/karaoke" element={<Karaoke />} />
-          <Route path="/progressbar" element={<ProgressBar />} />
-          <Route path="/lyricsdisplay" element={<LyricsDisplay />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueueProvider> {/* Wrap the application with the QueueProvider */}
+      <Router>
+        <div className="w-full p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/karaoke" element={<Karaoke />} />
+            <Route path="/progressbar" element={<ProgressBar />} />
+            <Route path="/lyricsdisplay" element={<LyricsDisplay />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueueProvider>
   );
 };
+
 export default App
