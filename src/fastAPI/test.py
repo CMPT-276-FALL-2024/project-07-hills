704d41cc3d336dcf8571e13c645dc0f16938fca6
An B. Luu <davidpro1080@gmail.com>
2024-11-23 14:05:15 -0800

    updated sample

--- src/fastAPI/test.py
+++ src/fastAPI/test.py
@@ -2,12 +2,15 @@
 from spotify import *
 import json
 from Song import sanitize_filename
-
+from Song import *
 
 spotify_diy = SpotifyDIY(env_file="python.env")
-track = spotify_diy.get_single_track("love story")
+track = spotify_diy.get_single_track("creep")
 
-print(track)
+lyrics = None
+song = Song.create_from_track(track, lyrics)
+song.get_audio()
+print(song)
 # print(track)
 
 # lyrics = spotify_diy.get_lyrics(track)
